da4c47c411a807502c0269bee4f508e33418b088
Diego Nunez <90226979+xDiegoNunezx@users.noreply.github.com>
2022-12-02 20:57:35 -0600

    analizadorSintactico.h comentado

--- src/analizadorSintactico.h
+++ src/analizadorSintactico.h
@@ -1,3 +1,10 @@
+/*
+Autores: Ricardo López Becerra, Diego Ignacio Nuñez Hernández
+Fecha de creación: 1/12/2022
+
+En este archivo se define el prototipo de la función encargada de iniciar
+el análisis sintáctico.
+*/
 #ifndef ANALIZADOR_SINTACTICO_H
 #define ANALIZADOR_SINTACTICO_H
 #include <stdio.h>
